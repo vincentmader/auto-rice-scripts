13b4d355c9efce151388bb75c695941d5e9794b0
Vincent C. Mader <vincent@mader.xyz>
2022-01-09 22:09:30 +0000

    minor

--- config/git/pull_git_repos.sh
+++ config/git/pull_git_repos.sh
@@ -16,13 +16,27 @@ if [ $OS = "arch" ]; then
     WALLPAPER="macOS/10-6_Snow-Leopard_Server-6k.jpg"
     feh --bg-scale "$PATH_TO_WALLPAPERS/$WALLPAPER"
     
-    # TODO get code projects
-    URL_TO_MXYZ="https://github.com/vincentmader/mader.xyz"
-    git clone $URL_TO_MXYZ $CONF/symlinks/code/mader.xyz
+    # get code projects  (TODO get others)
+    PATH_TO_CODE="$CONF/symlinks/code"
+
+    PATH_TO_MXYZ="$PATH_TO_CODE/mader.xyz"
+    if [ -d $PATH_TO_MXYZ ]; then
+        cd $PATH_TO_MXYZ
+        git pull
+    else
+        URL_TO_MXYZ="https://github.com/vincentmader/mader.xyz"
+        git clone $URL_TO_MXYZ $PATH_TO_MXYZ
+    fi
     
     # get org files
-    URL_TO_ORG="https://github.com/vincentmader/org"
-    git clone $URL_TO_ORG $HOME/org
+    PATH_TO_ORG="$HOME/org"
+    if [ -d $PATH_TO_ORG ]; then
+        cd $PATH_TO_ORG
+        git pull
+    else
+        URL_TO_ORG="https://github.com/vincentmader/org"
+        git clone $URL_TO_ORG $PATH_TO_ORG
+    fi
 
 elif [ $OS = "macOS" ]; then
     echo "repo sync is done automatically on macOS (via iCloud)"
