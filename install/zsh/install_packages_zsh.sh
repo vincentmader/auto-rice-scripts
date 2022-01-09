747358fde612ceeb3c0c126f0797c3004dba1afa
Vincent C. Mader <vincent@mader.xyz>
2022-01-09 19:16:48 +0000

    minor

--- install/zsh/install_packages_zsh.sh
+++ install/zsh/install_packages_zsh.sh
@@ -6,10 +6,11 @@ sudo rm -r $CONF/zsh/oh-my-zsh >> /dev/null;
 
 # install oh-my-zsh package manager
 export RUNZSH=no;
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+URL_TO_INSTALL="https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
+sh -c "$(curl -fsSL $URL_TO_INSTALL)";
 
 # delete newly-created $HOME/.zshrc files (& use my own instead)
-rm $HOME/.zshrc $HOME/.zshrc.pre-oh-my-zsh*;
+rm $HOME/.zshrc $HOME/.zshrc.pre-oh-my-zsh* 2> /dev/null;
 ln -s $CONF/zsh/zshrc $HOME/.zshrc
 
 # install syntax highlighting
