df5e58fce013cda0f53930d7de4e0ab5b6ceb879
Vincent C. Mader <vincent@mader.xyz>
2022-01-09 19:55:21 +0000

    oh-my-zsh install fix

--- install/zsh/install_packages_zsh.sh
+++ install/zsh/install_packages_zsh.sh
@@ -9,10 +9,16 @@ export RUNZSH=no;
 URL_TO_INSTALL="https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
 sh -c "$(curl -fsSL $URL_TO_INSTALL)";
 
+if [ "$OS" = "arch" ]; then
+    mv $HOME/.oh-my-zsh $CONF/zsh/oh-my-zsh
+fi
+
 # delete newly-created $HOME/.zshrc files (& use my own instead)
 rm $HOME/.zshrc $HOME/.zshrc.pre-oh-my-zsh* 2> /dev/null;
 ln -s $CONF/zsh/zshrc $HOME/.zshrc
 
 # install syntax highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $CONF/zsh/oh-my-zsh/plugins/zsh-syntax-highlighting;
+URL_TO_HIGHLIGHTING="https://github.com/zsh-users/zsh-syntax-highlighting.git"
+PATH_TO_HIGHLIGHTING="$CONF/zsh/oh-my-zsh/plugins/zsh-syntax-highlighting"
+git clone $URL_TO_HIGHLIGHTING $PATH_TO_HIGHLIGHTING;
 
