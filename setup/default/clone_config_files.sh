a80c9320b8d8070071d82deb93bdfdefad4312f3
Vincent C. Mader <vincent@mader.xyz>
2022-01-09 20:06:33 +0000

    updated git repo clone script

--- setup/default/clone_config_files.sh
+++ setup/default/clone_config_files.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 
-if [ ! -d $CONF ]; then
+if [ -d $CONF ]; then
+
+    cd $CONF
+    git pull
+
+else
 
     URL_TO_DOTFILES="https://github.com/vincentmader/config-dotfiles"
     git clone --recurse-submodules $URL_TO_DOTFILES $CONF
