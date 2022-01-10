f2982e3ef1220c60ee75a6ca3047c375667d29d9
Vincent C. Mader <vincent@mader.xyz>
2022-01-10 14:58:48 +0000

    minor

--- install/default/install_rust.sh
+++ install/default/install_rust.sh
@@ -2,20 +2,20 @@
 
 
 # uninstall rust (if installed)
-# if command -v "rustup" &> /dev/null; then
-#     rustup self uninstall
-#     # brew uninstall rust  # TODO needed?
-# fi
+if command -v "rustup" &> /dev/null; then
+    rustup self uninstall
+    # brew uninstall rust  # TODO needed?
+fi
 
-# # download & install base rust
-# echo "Installing rust..."
-# echo "NOTE: choose nightly build"  # TODO
-# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# download & install base rust
+echo "Installing rust..."
+echo "NOTE: choose nightly build"  # TODO
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-# # use nightly rust build as default
-# rustup toolchain install nightly
-# rustup install nightly   # TODO needed?
-# rustup default nightly
+# use nightly rust build as default
+rustup toolchain install nightly
+rustup install nightly   # TODO needed?
+rustup default nightly
 
 # install wasm-bindgen
 source $HOME/.cargo/env
@@ -23,5 +23,6 @@ cargo install -f wasm-bindgen-cli
 rustup target add wasm32-unknown-unknown
 
 # install language server stuff
-rustup component add rls rust-analysis rust-src
+rustup component add rust-analysis rust-src  # rls  TODO not yet possible :(
+
 
