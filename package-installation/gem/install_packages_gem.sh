ef4616dcabe574c9444f3c91bd03f9ec2216f0f9
Vincent C. Mader <vincent@mader.xyz>
2022-04-01 13:29:34 +0200

    added ruby-gem installs

---
+++ package-installation/gem/install_packages_gem.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+
+echo "$COLOR_BLUE\nInstalling ruby packages (gems)...$COLOR_DEFAULT"
+
+TO_INSTALL="$RICE/package-installation/gem/pkgs_gem/to-install.txt"
+ALREADY_INSTALLED="$RICE/package-installation/gem/pkgs_gem/installed.txt"
+
+function INSTALLER {
+    sudo gem install "$1"
+}
+
+install_from_pkg_file INSTALLER "$TO_INSTALL" "$ALREADY_INSTALLED"
+
