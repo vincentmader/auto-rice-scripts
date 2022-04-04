6414bd42383a89737f213cf18a2650ebdfc8a951
Vincent C. Mader <vincent@mader.xyz>
2022-04-04 13:27:39 +0200

    .

--- package-installation/os_arch/install_packages_arch.sh
+++ package-installation/os_arch/install_packages_arch.sh
@@ -4,12 +4,14 @@
 echo "$COLOR_BLUE\nInstalling arch-specific packages...$COLOR_DEFAULT"
 
 TO_INSTALL="$RICE/package-installation/os_arch/pkgs_arch/to-install.txt"
-ALREADY_INSTALLED="$RICE/package-installation/os_arch/pkgs_arch/installed.txt"
 
-function INSTALLER {
-    $PACKAGE_MANAGER "$1"
-    # $PACKAGE_MANAGER $(cat $RICE/package-installation/os_arch/pkgs-to-install_arch.txt)
-}
+# ALREADY_INSTALLED="$RICE/package-installation/os_arch/pkgs_arch/installed.txt"
+# function INSTALLER {
+#     $PACKAGE_MANAGER "$1"
+#     # $PACKAGE_MANAGER $(cat $RICE/package-installation/os_arch/pkgs-to-install_arch.txt)
+# }
 
-install_from_pkg_file INSTALLER "$TO_INSTALL" "$ALREADY_INSTALLED"
+$PACKAGE_MANAGER $(cat $TO_INSTALL)
+
+# TODO skip already installed?
 
